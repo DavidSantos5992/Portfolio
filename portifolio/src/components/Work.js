2517733e9f7b3122f4b352e360980a6ca4b27e9c
David Santos <davidhenriquehy@gmail.com>
2023-07-03 21:52:28 -0300

    UPDATE

--- portifolio/src/components/Work.js
+++ portifolio/src/components/Work.js
@@ -8,20 +8,19 @@ import img3 from '../assets/portfolio-img3.png'
 const Work = () => {
   return <section className='section' id='work'>
     <div className="container mx-auto">
-      <div className='flex flex-col lg:flex-row gap-x-10'>
-        <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
-          <div className='flex-1 flex flex-col gap-y-10'>
+      <div className='flex flex-col lg:flex-row gap-x-10 bg-red-200'>
+        <div className='flex-1 flex flex-col mb-10 lg:mb-0 '>
+          <div className='flex-1 flex flex-col  '>
 
             <h2 className='h2 leading-tight text-accent'>my latest
-              <br />
               work
             </h2>
-            <p className='max-w-sm mb-16'>
+            <p className='max-w-sm'>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro aspernatur, quisquam numquam iste tenetur dicta ipsam, nulla atque placeat debitis, non a natus vitae nisi maxime expedita consequuntur qui! Tempora!</p>
-            <button className='btn btn-sm'> view all</button></div>
+            <button className='btn btn-sm '> view all</button></div>
           <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
             {/* overlay */}
-            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <div className='group-hover:bg-black/70 w-full h-full absolute  transition-all duration-300'></div>
             <div></div>
             {/* img */}
             <img className='group-hover:scale-125 transition-all duration-500'
@@ -40,7 +39,7 @@ const Work = () => {
         <div className='flex-1 flex flex-col gap-y-10'>
 
           <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <div className='group-hover:bg-black/70 w-full h-full absolute  transition-all duration-300'></div>
             <div></div>
             <img className='group-hover:scale-125 transition-all duration-500'
               src={img1}
@@ -53,7 +52,7 @@ const Work = () => {
             </div>
           </div>
           <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <div className='group-hover:bg-black/70 w-full h-full absolute  transition-all duration-300'></div>
             <div></div>
             <img className='group-hover:scale-125 transition-all duration-500'
               src={img1}
