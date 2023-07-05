7f27f1c520ea0c4ed2be9ced68e47df876b7d7a7
David Santos <davidhenriquehy@gmail.com>
2023-07-04 21:09:16 -0300

    complete work

--- portifolio/src/components/Nav.js
+++ portifolio/src/components/Nav.js
@@ -5,7 +5,9 @@ import { Link } from 'react-scroll'
 
 const Nav = () => {
   return (
-    <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden x-50'>
+
+    <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden x-50 z-50'>
+
       <div className="container mx-auto">
 
         <div className='w-full z-50 bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex items-center justify-between text-2xl text-white/50'>
@@ -14,7 +16,7 @@ const Nav = () => {
             activeClass='active'
             smooth={true}
             spy={true}
-            offset={-200}
+            offset={-2500}
             className='cursor-pointer rigth-0  w-[60px] h-[60px] flex z-50 items-center justify-center'>
             <BiHomeAlt />
           </Link>
--- portifolio/src/components/Work.js
+++ portifolio/src/components/Work.js
@@ -8,7 +8,12 @@ import img3 from '../assets/portfolio-img3.png'
 const Work = () => {
   return <section className='section' id='work'>
     <div className="container mx-auto">
-      <div className='flex flex-col lg:flex-row gap-x-10 bg-red-200'>
+      <motion.div
+        variants={fadeIn('left', 0.5)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{ once: false, amount: 0.3 }}
+        className='flex flex-col lg:flex-row gap-x-10 '>
         <div className='flex-1 flex flex-col mb-10 lg:mb-0 '>
           <div className='flex-1 flex flex-col  '>
 
@@ -27,27 +32,32 @@ const Work = () => {
               src={img1}
             />
             {/* pppp */}
-            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 '>
               <span className='text-gradient'>ui/ux design</span>
             </div>
             {/* title */}
-            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 '>
               <span className='text-3xl text-white'>project title</span>
             </div>
           </div>
         </div>
-        <div className='flex-1 flex flex-col gap-y-10'>
+        <motion.div
+          variants={fadeIn('left', 0.2)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.5}}
+          className='flex-1 flex flex-col gap-y-10'>
 
           <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
             <div className='group-hover:bg-black/70 w-full h-full absolute  transition-all duration-300'></div>
             <div></div>
             <img className='group-hover:scale-125 transition-all duration-500'
-              src={img1}
+              src={img2}
             />
-            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 '>
               <span className='text-gradient'>ui/ux design</span>
             </div>
-            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 '>
               <span className='text-3xl text-white'>project title</span>
             </div>
           </div>
@@ -55,18 +65,18 @@ const Work = () => {
             <div className='group-hover:bg-black/70 w-full h-full absolute  transition-all duration-300'></div>
             <div></div>
             <img className='group-hover:scale-125 transition-all duration-500'
-              src={img1}
+              src={img3}
             />
-            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 '>
               <span className='text-gradient'>ui/ux design</span>
             </div>
-            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 '>
               <span className='text-3xl text-white'>project title</span>
             </div>
           </div>
 
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   </section>;
 };
