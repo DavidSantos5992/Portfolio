306d12e39bfd63c7cd70c8e9cbc10b257969d86d
David Santos <davidhenriquehy@gmail.com>
2023-10-05 19:26:03 -0300

    correção mobile

--- portifolio/src/components/Contact.js
+++ portifolio/src/components/Contact.js
@@ -4,16 +4,17 @@ import { fadeIn } from '../variants'
 
 const Contact = () => {
   return (
+
     <section className='section py-16 lg:section' id='contact'>
-      <div className="container mx-auto">
-        <div className='flex flex-col lg:flex-row'>
+      <div className="container mx-auto   mb-[500px]">
+        <div className='flex flex-col lg:flex-row '>
 
           <motion.div
             variants={fadeIn('left', 0.6)}
             initial='hidden'
             whileInView={'show'}
             viewport={{ once: false, amount: 0.7 }}
-            className='flex-1 flex justify-start items-center'>
+            className='flex-1 flex justify-start items-center '>
             <div>
               <h4 className='text-xl uppercase text-accent font-medium mb-2 tracking-wide'>ENTRe EM CONTATO</h4>
               <h2 className='text-[45px] lg:text-[90ox] leading-none mb-12'>vamos <br /> trabalhar  juntos</h2>
@@ -25,8 +26,8 @@ const Contact = () => {
             variants={fadeIn('right', 0.6)}
             initial='hidden'
             whileInView={'show'}
-            viewport={{ once: false, amount: 0.7 }}
-            className='flex-1 items-center justify-center  border rounded-2xl flex flex-col gap-y-6 '>
+            viewport={{ once: false, amount: 0.5 }}
+            className='flex-1 items-center justify-center   border rounded-2xl flex flex-col gap-y-6 '>
 
             {/* <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all'
               type="text"
@@ -45,12 +46,13 @@ const Contact = () => {
             <a
               target='_blank'
               href='https://api.whatsapp.com/send?phone=5511998252377&text=Ol%C3%A1%2C%20vi%20seu%20portf%C3%B3lio%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es'
-              className='btn btn-sm flex items-center justify-center'>Trabalhe comigo
+              className='btn btn-sm flex items-center justify-center '>Trabalhe comigo
             </a>
           </motion.form>
 
         </div>
       </div>
+
     </section>
   )
 };
--- portifolio/tailwind.config.js
+++ portifolio/tailwind.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
@@ -20,7 +21,7 @@ module.exports = {
     extend: {
       colors: {
         primary: '#0a0a0a',
-        accent: '#B809C3',
+        accent: '#1209c3',
       },
       backgroundImage: {
         site: "url('./assets/site-bg.jpg')",
